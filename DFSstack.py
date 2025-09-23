9bf410c0517578984ed85f313648e94a6cedc276
Junyang Zhang <junyangz@caltech.edu>
2025-09-23 00:53:17 -0700

    stack

--- DFSstack.py
+++ DFSstack.py
@@ -310,4 +310,26 @@ class Solution:
                 continue
         return res+ sign * num # handle the last num and one num case
         #1+(4+5+2)-3
-        #
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+
+        stack = []
+
+        for c in s:
+            if c!="]":
+                stack.append(c) # add chars to the stack first
+            else:
+                substr = ""
+                while stack[-1] != "[": # Finding the current substr by popping
+                    substr = stack.pop() + substr
+                stack.pop() #pop "["
+
+                n = ""
+                while stack and stack[-1].isdigit():
+                    n = stack.pop() + n
+
+                stack.append(int(n) * substr) #replace the substr with the new expanded one
+
+        return "".join(stack)
+        
