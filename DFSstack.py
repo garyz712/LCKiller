86cb19243e4b8b5f9ab7f34571c059bef9e83c3a
Junyang Zhang <junyanz9@uci.edu>
2025-10-04 15:41:15 -0700

    mono stack + ans

--- DFSstack.py
+++ DFSstack.py
@@ -345,3 +345,23 @@ class Solution:
                 res[idx] = i-idx
             stack.append([i, t])
         return res
+
+
+#monotonic decreasing stack using answers (each value is used only twice)
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def next(self, price: int) -> int:
+        ans = 1
+        while self.stack and self.stack[-1][0] <= price:
+            ans += self.stack.pop()[1] #pop all previous values smaller that the current price to keep the stack decreasing
+        self.stack.append([price, ans])
+        return ans
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
