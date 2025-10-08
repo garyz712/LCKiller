97d9d551dd412bc4f799893b14927f83e4ff6e67
Junyang Zhang <junyanz9@uci.edu>
2025-10-08 16:27:08 -0700

    two pointers + two heaps

--- DijkstraHeapq.py
+++ DijkstraHeapq.py
@@ -264,3 +264,37 @@ class SmallestInfiniteSet:
 # obj = SmallestInfiniteSet()
 # param_1 = obj.popSmallest()
 # obj.addBack(num)
+
+
+# two heap + two pointers
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        total= 0
+        heapLow = []
+        heapHigh = []
+        if 2*candidates <  len(costs):
+            for i in range(0, candidates):
+                heapq.heappush(heapLow, costs[i])
+                heapq.heappush(heapHigh, costs[len(costs)-i-1])
+            l, r = candidates, len(costs)-candidates-1
+        else:
+            for i in range(len(costs)):
+                heapq.heappush(heapLow, costs[i])
+            for i in range(k):
+                total += heapq.heappop(heapLow)
+            return total
+        
+        #print(heapLow, heapHigh, costs)
+        for i in range(k):
+            #print(heapLow, heapHigh, costs)
+            if heapHigh == [] or (heapLow and heapLow[0] <= heapHigh[0]):   #heap[0] is the smallest
+                total += heapq.heappop(heapLow)               
+                if l<=r:
+                    heapq.heappush(heapLow, costs[l])
+                    l+=1
+            else:
+                total += heapq.heappop(heapHigh)
+                if l<=r:
+                    heapq.heappush(heapHigh, costs[r])
+                    r-=1
+        return total
