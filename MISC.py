da199a920559bcc34f9fa0338a29dc597d6ffda8
Junyang Zhang <junyanz9@uci.edu>
2025-10-15 00:57:35 -0700

    use input array with idx as hash map

--- MISC.py
+++ MISC.py
@@ -320,6 +320,33 @@ class Solution:
 
         return res
 
+        
+# use input array with idx as hash map
+class Solution:
+
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if nums[i]<0:
+                nums[i]=0
+
+        for i in range(len(nums)):
+
+            val = abs(nums[i])
+
+            if 0<val<=len(nums):
+                if nums[val-1]>0:
+                    nums[val-1] *= -1
+                elif nums[val-1]==0:
+                    nums[val-1] = -val
+
+            #print(nums)
+
+        for i in range(len(nums)):
+            if nums[i] >= 0:
+                return i+1
+        return len(nums)+1
+
+
 
 # hash map for prefix sum
 class Solution:
