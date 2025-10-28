b7c8ac4536ad789dfc700e77ba0f0fa4adcc22bd
Junyang <junyang@Junyangs-MacBook-Pro.local>
2025-10-28 13:26:25 -0700

    unsorted BS

--- BinarySearch.py
+++ BinarySearch.py
@@ -620,3 +620,15 @@ class Solution:
             slow2 = nums[slow2]
         
         return slow
+s
+# unsorted binary search + assume the array is ascending/descending at some point
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums)-1
+        while left<right:
+            mid = (left+right)//2
+            if nums[mid]>nums[mid+1]:
+                right=mid
+            else:
+                left=mid+1
+        return left
