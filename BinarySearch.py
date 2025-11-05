5e7a0bec82d8273dc4989d8fd1de82867d3a6e4d
Junyang <junyang@Junyangs-MacBook-Pro.local>
2025-11-05 01:38:09 -0800

    non standard binary search with == vs !=

--- BinarySearch.py
+++ BinarySearch.py
@@ -700,3 +700,21 @@ class Solution:
             else:
                 left=mid+1
         return left
+
+
+# non standard binary search with == vs != 
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        n = len(arr)
+        diff = (arr[-1] - arr[0]) // n
+        left, right = 0, n - 1
+
+        while left < right:
+            mid = (left + right) // 2
+            if arr[mid] == arr[0] + mid * diff:
+                left = mid + 1
+            else:
+                right = mid
+
+        # [100,300,400]
+        return arr[0] + left * diff
